e432fd36762006027f112a42bdec6ba4c7d59fa0
Dmitry Ermichev <dmitryermichev@gmail.com>
2019-10-01 15:10:27 +0300

    initial commit

---
+++ android/src/main/java/com/github/dmitryermichev/reactnative/yandexmapkit3/ReactNativeYandexmapkit3Module.java
@@ -0,0 +1,27 @@
+package com.github.dmitryermichev.reactnative.yandexmapkit3;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+public class ReactNativeYandexmapkit3Module extends ReactContextBaseJavaModule {
+
+    private final ReactApplicationContext reactContext;
+
+    public ReactNativeYandexmapkit3Module(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+    @Override
+    public String getName() {
+        return "ReactNativeYandexmapkit3";
+    }
+
+    @ReactMethod
+    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
+        // TODO: Implement some actually useful functionality
+        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    }
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+import {NativeModules} from 'react-native';
+
+const {ReactNativeYandexmapkit3} = NativeModules;
+
+export default ReactNativeYandexmapkit3;
